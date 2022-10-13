6f1463dff9fb3a367e6e42e2f1bbc7db96e99834
r4bb1t <r4bb1t.github@gmail.com>
2022-10-13 21:15:19 +0900

    add database setting

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log('Is this working?');
+import 'reflect-metadata';
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+export const AppDataSource = new DataSource({
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  synchronize: true,
+  logging: false,
+  entities: ['src/entities/*.ts'],
+  migrations: ['src/migrations/**/*.ts'],
+  subscribers: ['src/subscribers/**/*.subscriber.ts'],
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log('Data Source has been initialized!');
+  })
+  .catch((err) => {
+    console.error('Error during Data Source initialization:', err);
+  });
+
+const app: Application = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/', async (req: Request, res: Response) => {
+  res.send(200);
+});
+
+const PORT = 4000;
+
+try {
+  app.listen(PORT, (): void => {
+    console.log(`Connected successfully on port ${PORT}`);
+  });
+} catch (error: any) {
+  console.error(`Error occured: ${error.message}`);
+}
